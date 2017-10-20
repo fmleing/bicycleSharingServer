aad6277b4e643e1fe22a21593a54d7baa7d5cdc9
fmleing <fmleing@163.com>
2017-10-20 21:58:22 +0800

    admin实体类和DAO及映射配置文件

---
+++ src/main/java/com/bicyclesharing/admin/dao/AdminMapper.java
@@ -0,0 +1,17 @@
+package com.bicyclesharing.admin.dao;
+
+import com.bicyclesharing.admin.entity.Admin;
+
+public interface AdminMapper {
+    int deleteByPrimaryKey(Integer adminId);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    Admin selectByPrimaryKey(Integer adminId);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+}
