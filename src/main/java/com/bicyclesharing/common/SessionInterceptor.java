297f358cbb1853f58442f54767d5c86b8d364e90
fmleing <fmleing@163.com>
2017-10-21 12:48:04 +0800

    会话拦截器类

---
+++ src/main/java/com/bicyclesharing/common/SessionInterceptor.java
@@ -0,0 +1,46 @@
+package com.bicyclesharing.common;
+
+import com.bicyclesharing.user.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Tomecs on 2017/10/21.
+ */
+public class SessionInterceptor implements HandlerInterceptor {
+    /**
+     * 进行登陆拦截
+     * 这里我们先实现用户登陆,之后实现管理员
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String uri = httpServletRequest.getRequestURI();
+        if ((uri.indexOf("index_login") >= 0) || (uri.indexOf("error") >= 0)) {
+            return true;
+        }
+        User user = (User) httpServletRequest.getSession().getAttribute("userinfo");
+        if (user != null) {
+            return true;
+        }
+        httpServletRequest.getRequestDispatcher("/index_login").forward(httpServletRequest, httpServletResponse);
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
