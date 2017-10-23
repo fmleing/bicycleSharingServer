77032e376d67834a704004c8161e7961b7a823ce
fmleing <fmleing@163.com>
2017-10-23 18:52:44 +0800

    userfeadback添加方法

--- src/main/java/com/bicyclesharing/userfeedback/dao/UserFeedbackMapper.java
+++ src/main/java/com/bicyclesharing/userfeedback/dao/UserFeedbackMapper.java
@@ -2,6 +2,8 @@ package com.bicyclesharing.userfeedback.dao;
 
 import com.bicyclesharing.userfeedback.entity.UserFeedback;
 
+import java.util.List;
+
 public interface UserFeedbackMapper {
     int deleteByPrimaryKey(Integer feedbackId);
 
@@ -14,4 +16,35 @@ public interface UserFeedbackMapper {
     int updateByPrimaryKeySelective(UserFeedback record);
 
     int updateByPrimaryKey(UserFeedback record);
+
+    /**
+     * 1.添加用户反馈
+     * @param userFeedback
+     * @return
+     */
+    boolean insertUserFeedback(UserFeedback userFeedback);
+
+    /**
+     * 2.删除用户反馈
+     * @param userFeedbackId
+     * @return
+     */
+    boolean deleteUserFeedbackById(Integer userFeedbackId);
+
+    /**
+     * 3.修改用户反馈
+     * @param userFeedback
+     * @return
+     */
+    boolean updateUserFeedback(UserFeedback userFeedback);
+
+    /**
+     * 4.查询用户反馈
+     * @param userFeedbackId
+     * @return
+     */
+    UserFeedback selectUserFeedbackById(Integer userFeedbackId);
+    List<UserFeedback> selectUserFeedbackByUserId(Integer userId);
+    List<UserFeedback> selectUserFeedbackByBicycleId(Integer bicycleId);
+    List<UserFeedback> selectAllUserFeedback();
 }
