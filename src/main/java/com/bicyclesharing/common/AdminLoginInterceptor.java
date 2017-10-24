3ec489a3752ba3a16e63b8394dcb36bfa243e988
fmleing <fmleing@163.com>
2017-10-24 15:59:41 +0800

    拦截器

---
+++ src/main/java/com/bicyclesharing/common/AdminLoginInterceptor.java
@@ -0,0 +1,33 @@
+package com.bicyclesharing.common;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 登录验证拦截器
+ * Created by Tomecs on 2017/10/24.
+ */
+public class AdminLoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        HttpSession session = httpServletRequest.getSession();
+        if (session.getAttribute("admin") == null) {
+            httpServletResponse.sendRedirect("/admin/admin-index-login-show");
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
